28d2bd7f1b779b5f685841108989c16f920af032
aimantahir <aimantahir1225@gmail.com>
2023-05-26 11:41:20 +0500

    alarms

--- popup.js
+++ popup.js
@@ -1,21 +1,20 @@
 const timeElement = document.getElementById("time");
 const nameElement = document.getElementById("name");
-const currentTime = new Date().toLocaleTimeString();
-timeElement.textContent = `The time is: ${currentTime}`;
+const timerElement = document.getElementById("timer");
 
-chrome.action.setBadgeText( 
-    //The setBadgeText method is a function commonly used in browser extensions to update the text displayed on the extension's badge. 
-    //The badge refers to a small icon or text displayed on the browser's toolbar or next to the extension's icon.
-  {
-    text: "TIME",
-  },
-  () => {
-    console.log("Finished setting badge text");
-  }
-);
+function updateTimeElements() {
+  chrome.storage.local.get(["timer"], (res) => {
+    const time = res.timer ?? 0;
+    timerElement.textContent = `The timer is at: ${time} seconds`;
+  });
 
-chrome.storage.sync.get(["name"],(res)=>{
+  const currentTime = new Date().toLocaleTimeString();
+  timeElement.textContent = `The time is: ${currentTime}`;
+}
+
+updateTimeElements();
+setInterval(updateTimeElements,1000);
+chrome.storage.sync.get(["name"], (res) => {
   const name = res.name ?? "???";
-  nameElement.textContent = `Your name is: ${name}`
-})
-console.log(this);
+  nameElement.textContent = `Your name is: ${name}`;
+});
